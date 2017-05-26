fd72d121c6ff397db98c5420785344b4dfca079d
cpestoury <cp@make.org>
2017-05-26 12:01:11 +0200

    Optimize imports

--- src/main/scala/org/make/front/Main.scala
+++ src/main/scala/org/make/front/Main.scala
@@ -1,19 +1,18 @@
 package org.make.front
 
 import io.github.shogowada.scalajs.history.History
-import io.github.shogowada.scalajs.reactjs.{React, ReactDOM}
 import io.github.shogowada.scalajs.reactjs.VirtualDOM._
 import io.github.shogowada.scalajs.reactjs.classes.ReactClass
 import io.github.shogowada.scalajs.reactjs.events.SyntheticEvent
 import io.github.shogowada.scalajs.reactjs.redux.ReactRedux._
 import io.github.shogowada.scalajs.reactjs.redux.Redux.Dispatch
-import io.github.shogowada.scalajs.reactjs.redux.{NativeAction, ReactRedux, Redux}
 import io.github.shogowada.scalajs.reactjs.redux.devtools.ReduxDevTools
+import io.github.shogowada.scalajs.reactjs.redux.{NativeAction, ReactRedux, Redux}
 import io.github.shogowada.scalajs.reactjs.router.Router._
 import io.github.shogowada.scalajs.reactjs.router.RouterProps
-import io.github.shogowada.scalajs.reactjs.router.redux.{ReactRouterRedux, ReactRouterReduxAction}
+import io.github.shogowada.scalajs.reactjs.router.redux.ReactRouterRedux
 import io.github.shogowada.scalajs.reactjs.router.redux.ReactRouterRedux.RouterReduxVirtualDOMElements
-import io.github.shogowada.scalajs.reactjs.router.redux.ReactRouterReduxAction._
+import io.github.shogowada.scalajs.reactjs.{React, ReactDOM}
 import org.make.front.FrontPage.FrontPageDebateProps
 import org.scalajs.dom
 
@@ -77,7 +76,7 @@ object Main extends JSApp {
 
     lazy val reactClass: ReactClass = React.createClass[WrappedProps, Unit](_ =>
       <.Switch()(
-        <.Route(^.path := "/",           ^.component := FrontPage.debateContainer)(),
+        <.Route(^.path := "/",           ^.component := FrontPage.debateContainer)()
         <.Route(^.path := "/debate/:id", ^.component := BRouteComponent.reactClass)()
       )
     )
