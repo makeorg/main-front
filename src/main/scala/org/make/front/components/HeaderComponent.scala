73e55c4aa10f74fd9a16fa434f1300086a21e176
Amine Zaghdoudi <amine.zaghdoudi@gmail.com>
2017-07-19 16:57:27 +0200

    feat(layout): integrate scala css
    close #21

--- src/main/scala/org/make/front/components/HeaderComponent.scala
+++ src/main/scala/org/make/front/components/HeaderComponent.scala
@@ -2,15 +2,33 @@ package org.make.front.components
 
 import io.github.shogowada.scalajs.reactjs.React
 import io.github.shogowada.scalajs.reactjs.VirtualDOM._
+import scalacss.DevDefaults._
+import scalacss.internal.mutable.StyleSheet
 
 object HeaderComponent {
   def apply() = reactClass
 
   private lazy val reactClass = React.createClass[Unit, Unit](
     render = (_) =>
-      <.div()(
-        "The header of the page"
+      <.div(^.className := HeaderStyles.common.htmlClass)(
+        "The header of the page",
+        <.style()(
+          HeaderStyles.render[String]
+        )
       )
+
+  )
+
+}
+
+object HeaderStyles extends StyleSheet.Inline {
+  import dsl._
+
+  val common = style(
+    backgroundColor.green,
+    media.maxWidth(320 px)(
+      backgroundColor.red
+    )
   )
 
 }
