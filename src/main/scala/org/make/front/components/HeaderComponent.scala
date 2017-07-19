337414d4c407ad335d514f9cb510ff8dc5811648
Amine Zaghdoudi <amine.zaghdoudi@gmail.com>
2017-07-19 18:29:13 +0200

    feat(header): add styles and logo for header

--- src/main/scala/org/make/front/components/HeaderComponent.scala
+++ src/main/scala/org/make/front/components/HeaderComponent.scala
@@ -2,6 +2,7 @@ package org.make.front.components
 
 import io.github.shogowada.scalajs.reactjs.React
 import io.github.shogowada.scalajs.reactjs.VirtualDOM._
+
 import scalacss.DevDefaults._
 import scalacss.internal.mutable.StyleSheet
 
@@ -9,26 +10,44 @@ object HeaderComponent {
   def apply() = reactClass
 
   private lazy val reactClass = React.createClass[Unit, Unit](
-    render = (_) =>
-      <.div(^.className := HeaderStyles.common.htmlClass)(
-        "The header of the page",
+    render = (self) =>
+      <.div(^.className := HeaderStyles.container.htmlClass)(
+        <.div(^.className := HeaderStyles.header_container.htmlClass)(
+          <.a(^.className := HeaderStyles.header_logo_container.htmlClass, ^.href := "/")(
+            <.img(^.src := "http://phantom.make.org/images/logoMake.svg")()
+          ),
+          <(SearchInputComponent()).empty,
+          <(UserHeaderComponent()).empty
+        ),
         <.style()(
           HeaderStyles.render[String]
         )
       )
-
   )
-
 }
 
 object HeaderStyles extends StyleSheet.Inline {
   import dsl._
 
-  val common = style(
-    backgroundColor.green,
-    media.maxWidth(320 px)(
-      backgroundColor.red
-    )
+  val container: StyleA = style(
+    position.relative,
+    zIndex(500),
+    height(80.px),
+    padding(12.px,  50.px, 0.px, 50.px),
+    backgroundColor(c"#fff"),
+    boxShadow := "1px 1px 4px 0 rgba(0, 0, 0, 0.5)"
   )
 
+  val header_container: StyleA = style(
+    position.relative,
+    width(100.%%),
+    height(100.%%),
+    maxWidth(1140.px),
+    marginRight(auto),
+    marginLeft(auto)
+  )
+
+  val header_logo_container: StyleA = style(
+    float.left
+  )
 }
