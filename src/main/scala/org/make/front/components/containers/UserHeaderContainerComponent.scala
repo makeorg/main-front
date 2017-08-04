4954bfa999ea11e358198456a6a11bc632ece45c
csalmon- <salmonl.colin@gmail.com>
2017-08-04 09:37:36 +0200

    refactor(user): change image link to use webpack hash one

--- src/main/scala/org/make/front/components/containers/UserHeaderContainerComponent.scala
+++ src/main/scala/org/make/front/components/containers/UserHeaderContainerComponent.scala
@@ -1,6 +1,7 @@
 package org.make.front.components.containers
 
 import io.github.shogowada.scalajs.reactjs.React.Props
+import io.github.shogowada.scalajs.reactjs.classes.ReactClass
 import io.github.shogowada.scalajs.reactjs.redux.ReactRedux
 import io.github.shogowada.scalajs.reactjs.redux.Redux.Dispatch
 import io.github.shogowada.scalajs.reactjs.router.RouterProps
@@ -9,7 +10,7 @@ import org.make.front.models.AppState
 
 object UserHeaderContainerComponent extends RouterProps {
 
-  lazy val reactClass = ReactRedux.connectAdvanced { _: Dispatch => (state: AppState, _: Props[Unit]) =>
+  lazy val reactClass: ReactClass = ReactRedux.connectAdvanced { _: Dispatch => (state: AppState, _: Props[Unit]) =>
     UserHeaderComponent.WrappedProps(
       isConnected = state.connectedUser.isDefined,
       userFirstName = state.connectedUser.map(_.firstname),
