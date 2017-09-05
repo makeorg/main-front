426fc7b0ac2fa320f9532a3e3b296a9a79333ede
csalmon- <salmonl.colin@gmail.com>
2017-09-05 09:08:23 +0200

    refactor(proposal): add TagId and rename postProposal to createProposal

--- src/main/scala/org/make/front/components/presentationals/ProposalMatrixComponent.scala
+++ src/main/scala/org/make/front/components/presentationals/ProposalMatrixComponent.scala
@@ -16,73 +16,72 @@ import scalacss.internal.mutable.StyleSheet
 object ProposalMatrixComponent {
 
   val proposals: Seq[Proposal] = {
-    for( i <- 1 to 20) yield Proposal(
-      id = ProposalId("abcd"),
-      userId = UserId("asdf"),
-      content = "proposal content fetched from API",
-      slug = "proposal",
-      status = "",
-      createdAt = ZonedDateTime.now(),
-      updatedAt = None,
-      voteAgree = Vote(
-        key = "agree",
-        count = 2500,
-        qualifications = Seq(
-          Qualification(key = "likeIt", count = 952),
-          Qualification(key = "doable", count = 97),
-          Qualification(key = "platitudeAgree", count = 7)
+    for (i <- 1 to 20)
+      yield
+        Proposal(
+          id = ProposalId("abcd"),
+          userId = UserId("asdf"),
+          content = "proposal content fetched from API",
+          slug = "proposal",
+          status = "",
+          createdAt = ZonedDateTime.now(),
+          updatedAt = None,
+          voteAgree = Vote(
+            key = "agree",
+            count = 2500,
+            qualifications = Seq(
+              Qualification(key = "likeIt", count = 952),
+              Qualification(key = "doable", count = 97),
+              Qualification(key = "platitudeAgree", count = 7)
+            )
+          ),
+          voteDisagree = Vote(
+            key = "disagree",
+            count = 660,
+            qualifications = Seq(
+              Qualification(key = "noWay", count = 320),
+              Qualification(key = "impossible", count = 53),
+              Qualification(key = "platitudeDisagree", count = 9)
+            )
+          ),
+          voteNeutral = Vote(
+            key = "neutral",
+            count = 170,
+            qualifications = Seq(
+              Qualification(key = "doNotUnderstand", count = 74),
+              Qualification(key = "noOpinion", count = 12),
+              Qualification(key = "doNotCare", count = 3)
+            )
+          ),
+          proposalContext = ProposalContext(operation = None, source = None, location = None, question = None),
+          trending = None,
+          labels = Seq.empty,
+          author = Author(firstname = Some("Marco"), postalCode = Some("75"), age = Some(42)),
+          country = "FR",
+          language = "fr",
+          themeId = Some(ThemeId("1")),
+          tags = Seq(
+            Tag(TagId("tag-un"), label = "un"),
+            Tag(TagId("tag-deux"), label = "deux"),
+            Tag(TagId("tag-trois"), label = "trois")
+          )
         )
-      ),
-      voteDisagree = Vote(
-        key = "disagree",
-        count = 660,
-        qualifications = Seq(
-          Qualification(key = "noWay", count = 320),
-          Qualification(key = "impossible", count = 53),
-          Qualification(key = "platitudeDisagree", count = 9)
-        )
-      ),
-      voteNeutral = Vote(
-        key = "neutral",
-        count = 170,
-        qualifications = Seq(
-          Qualification(key = "doNotUnderstand", count = 74),
-          Qualification(key = "noOpinion", count = 12),
-          Qualification(key = "doNotCare", count = 3)
-        )
-      ),
-      proposalContext = ProposalContext(operation = None, source = None, location = None, question = None),
-      trending = None,
-      labels = Seq.empty,
-      author = Author(firstname = Some("Marco"), postalCode = Some("75"), age = Some(42)),
-      country = "FR",
-      language = "fr",
-      themeId = Some(ThemeId("1")),
-      tags = Seq(Tag(name = "un"), Tag(name = "deux"), Tag(name = "trois"))
-    )
   }
 
   lazy val reactClass: ReactClass = React.createClass[Unit, Unit](
     render = (self) =>
       <.div()(
-        <.h2(^.className := AppComponentStyles.title2)(
-          <.Translate(
-            ^.value := "content.theme.matrix.title"
-          )()
-        ),
+        <.h2(^.className := AppComponentStyles.title2)(<.Translate(^.value := "content.theme.matrix.title")()),
         <.TagFilterComponent()(),
         <.div(^.className := ProposalMatrixStyles.proposalList)(
           <.div(^.className := Seq(BulmaStyles.Grid.Columns.columns, BulmaStyles.Grid.Columns.columnsMultiLines))(
-            proposals.map( proposal =>
-              <.ProposalTileComponent(
-                ^.wrapped := ProposalTileComponent.ProposalTileProps(
-                  proposal = proposal
-                )
-              )()
+            proposals.map(
+              proposal =>
+                <.ProposalTileComponent(^.wrapped := ProposalTileComponent.ProposalTileProps(proposal = proposal))()
             )
-        )
-      ),
-      <.style()(ProposalMatrixStyles.render[String])
+          )
+        ),
+        <.style()(ProposalMatrixStyles.render[String])
     )
   )
 }
@@ -90,7 +89,5 @@ object ProposalMatrixComponent {
 object ProposalMatrixStyles extends StyleSheet.Inline {
   import dsl._
 
-  val proposalList: StyleA = style(
-    marginTop(4.rem)
-  )
+  val proposalList: StyleA = style(marginTop(4.rem))
 }
--- src/main/scala/org/make/front/components/presentationals/TagFilterComponent.scala
+++ src/main/scala/org/make/front/components/presentationals/TagFilterComponent.scala
@@ -3,15 +3,15 @@ package org.make.front.components.presentationals
 import io.github.shogowada.scalajs.reactjs.React
 import io.github.shogowada.scalajs.reactjs.VirtualDOM.{<, _}
 import io.github.shogowada.scalajs.reactjs.classes.ReactClass
+import io.github.shogowada.statictags.Element
 import org.make.front.components.AppComponentStyles
 import org.make.front.components.presentationals.TagListComponent.TagListComponentProps
 import org.make.front.facades.Translate.TranslateVirtualDOMElements
-import org.make.front.models.Tag
+import org.make.front.models.{Tag, TagId}
 import org.make.front.styles.{BulmaStyles, FontAwesomeStyles}
 
 import scalacss.DevDefaults._
 import scalacss.internal.mutable.StyleSheet
-
 
 /**
   * Generates the Tag filter
@@ -20,8 +20,11 @@ import scalacss.internal.mutable.StyleSheet
   *
   * - Creates a tag filter
   *
-  * <.TagFilterComponent(
-  *     ^.wrapped := TagFilterComponentProps(tags = Seq(Tag("hello"), Tag("world")), toggleShowAll = false)
+  * <.TagListComponent(
+  *    ^.wrapped := TagListComponentProps(
+  *      tags = Seq(Tag(TagId("tag-hello"), "hello"), Tag(TagId("tag-world"), "world")),
+  *      toggleShowAll = false
+  *    )
   *  )()
   *
   */
@@ -33,63 +36,56 @@ object TagFilterComponent {
   lazy val reactClass: ReactClass =
     React.createClass[TagFilterComponentProps, TagFilterComponentState](
       getInitialState = (_) => TagFilterComponentState(showAll = false, selectedTags = Seq.empty),
-        render = (self) => {
-          def handleSelectedTags(tag: Tag) = {
-            self.setState((previousState: TagFilterComponentState) => TagFilterComponentState(
-              selectedTags = {
+      render = (self) => {
+        def handleSelectedTags(tag: Tag): Unit = {
+          self.setState(
+            (previousState: TagFilterComponentState) =>
+              TagFilterComponentState(selectedTags = {
                 if (previousState.selectedTags.contains(tag)) {
                   previousState.selectedTags.filter(_ != tag)
                 } else {
                   previousState.selectedTags :+ tag
                 }
-              },
-              showAll = previousState.showAll
-            ))
-          }
-
-          <.div(^.className := BulmaStyles.Element.isClipped)(
-            <.ul()(
-              self.state.selectedTags.map(tag => {
-                <.li()(tag.name)
-              })
-            ),
-            <.div(^.className := BulmaStyles.Helpers.isPulledLeft)(
-              <.span(^.className := Seq(AppComponentStyles.icon, BulmaStyles.Helpers.isSmall))(
-                <.i(^.className := FontAwesomeStyles.lineChart)()
-              ),
-              <.Translate(
-                ^.value := "content.theme.matrix.filter.tag.title"
-              )()
-            ),
-            <.div(^.className := Seq(BulmaStyles.Helpers.isPulledLeft, TagFilterStyles.tagsList))(
-              <.TagListComponent(
-                ^.wrapped := TagListComponentProps(
-                  tags = Seq(
-                    Tag("budget"),
-                    Tag("équipement"),
-                    Tag("formation"),
-                    Tag("Alimentation"),
-                    Tag("Bio"),
-                    Tag("viande"),
-                    Tag("Permaculture")
-                  ),
-                  withShowMoreButton = true,
-                  handleSelectedTags = handleSelectedTags
-                )
-              )()
-            ),
-            <.style()(TagFilterStyles.render[String])
+              }, showAll = previousState.showAll)
           )
         }
-      )
-}
 
+        val tagList = TagListComponentProps(
+          tags = Seq(
+            Tag(TagId("tag-budget"), "budget"),
+            Tag(TagId("tag-equipement"), "équipement"),
+            Tag(TagId("tag-formation"), "formation"),
+            Tag(TagId("tag-alimentation"), "Alimentation"),
+            Tag(TagId("tag-bio"), "Bio"),
+            Tag(TagId("tag-viande"), "viande"),
+            Tag(TagId("tag-permaculture"), "Permaculture")
+          ),
+          withShowMoreButton = true,
+          handleSelectedTags = handleSelectedTags
+        )
+        val selectedTags: Seq[Element] = self.state.selectedTags.map(tag => {
+          <.li()(tag.label)
+        })
+        <.div(^.className := BulmaStyles.Element.isClipped)(
+          <.ul()(selectedTags),
+          <.div(^.className := BulmaStyles.Helpers.isPulledLeft)(
+            <.span(^.className := Seq(AppComponentStyles.icon, BulmaStyles.Helpers.isSmall))(
+              <.i(^.className := FontAwesomeStyles.lineChart)()
+            ),
+            <.Translate(^.value := "content.theme.matrix.filter.tag.title")()
+          ),
+          <.div(^.className := Seq(BulmaStyles.Helpers.isPulledLeft, TagFilterStyles.tagsList))(
+            <.TagListComponent(^.wrapped := tagList)()
+          ),
+          <.style()(TagFilterStyles.render[String])
+        )
+      }
+    )
+}
 
 object TagFilterStyles extends StyleSheet.Inline {
   import dsl._
 
-  val tagsList: StyleA = style(
-    marginLeft(2.rem)
-  )
+  val tagsList: StyleA = style(marginLeft(2.rem))
 
 }
