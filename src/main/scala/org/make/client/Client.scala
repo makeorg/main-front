3360106373809877717f11cf4dd2705da7408825
csalmon- <salmonl.colin@gmail.com>
2017-09-05 22:10:01 +0200

    feat(client): error custom handler

--- src/main/scala/org/make/client/Client.scala
+++ src/main/scala/org/make/client/Client.scala
@@ -1,6 +1,8 @@
 package org.make.client
 
 import io.circe.Decoder
+import io.circe.generic.auto._
+import io.circe.syntax._
 import org.scalajs.dom.ext.Ajax.InputData
 
 import scala.concurrent.Future
@@ -26,8 +28,27 @@ trait Client {
                     data: InputData,
                     headers: Map[String, String] = Map.empty)(implicit decoder: Decoder[ENTITY]): Future[Option[ENTITY]]
 
-  def delete[ENTITY](apiEndpoint: String,
-                     urlParams: Seq[(String, Any)],
-                     data: InputData,
-                     headers: Map[String, String] = Map.empty)(implicit decoder: Decoder[ENTITY]): Future[Option[ENTITY]]
+  def delete[ENTITY](
+    apiEndpoint: String,
+    urlParams: Seq[(String, Any)],
+    data: InputData,
+    headers: Map[String, String] = Map.empty
+  )(implicit decoder: Decoder[ENTITY]): Future[Option[ENTITY]]
 }
+
+trait HttpException extends Exception
+
+case class ValidationError(field: String, message: Option[String])
+
+trait ValidationFailedHttpException extends HttpException {
+  val errors: Seq[ValidationError]
+  override def getMessage: String = { errors.asJson.toString }
+}
+
+case class BadRequestHttpException(override val errors: Seq[ValidationError]) extends ValidationFailedHttpException
+case object UnauthorizedHttpException extends HttpException
+case object ForbiddenHttpException extends HttpException
+case object NotFoundHttpException extends HttpException
+case object InternalServerHttpException extends HttpException
+case object BadGatewayHttpException extends HttpException
+case object NotImplementedHttpException extends HttpException
