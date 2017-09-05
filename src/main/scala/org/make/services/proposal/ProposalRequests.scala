1ab3145c0c97e58f9c9b0ff0140aa60fcc0fc64e
csalmon- <salmonl.colin@gmail.com>
2017-09-05 09:43:54 +0200

    feat(proposal): service method to search proposals
    Add request models

--- src/main/scala/org/make/services/proposal/ProposalRequests.scala
+++ src/main/scala/org/make/services/proposal/ProposalRequests.scala
@@ -1,5 +1,28 @@
 package org.make.services.proposal
 
-object ProposalRequests {
-  final case class RegisterProposalRequest(content: String)
+import io.circe.{Decoder, Encoder, Json}
+
+final case class RegisterProposalRequest(content: String)
+sealed trait Order { val shortName: String }
+
+case object OrderAsc extends Order { override val shortName: String = "ASC" }
+case object OrderDesc extends Order { override val shortName: String = "DESC" }
+
+object Order {
+  implicit lazy val orderEncoder: Encoder[Order] = (order: Order) => Json.fromString(order.shortName)
+  implicit lazy val orderDecoder: Decoder[Order] = Decoder.decodeString.map(
+    order => matchOrder(order).getOrElse(throw new IllegalArgumentException(s"$order is not a Order"))
+  )
+
+  val orders: Map[String, Order] = Map(OrderAsc.shortName -> OrderAsc, OrderDesc.shortName -> OrderDesc)
+
+  def matchOrder(order: String): Option[Order] =
+    orders.get(order.toUpperCase)
 }
+
+final case class SortOptionRequest(field: String, mode: Option[Order])
+final case class SearchOptionsRequest(sort: Seq[SortOptionRequest], limit: Option[Int], skip: Option[Int])
+final case class SearchRequest(themesIds: Option[Seq[String]] = None,
+                               tagsIds: Option[Seq[String]] = None,
+                               content: Option[String] = None,
+                               options: Option[SearchOptionsRequest] = None)
