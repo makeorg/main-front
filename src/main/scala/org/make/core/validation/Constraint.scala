af47c84b7409deee166f3a359769e9263374bae7
Philippe de Araujo <pa@make.org>
2017-09-06 15:18:46 +0200

    fix(proposal): fix and refactor proposal validation

--- src/main/scala/org/make/core/validation/Constraint.scala
+++ src/main/scala/org/make/core/validation/Constraint.scala
@@ -3,7 +3,7 @@ package org.make.core.validation
 case class ConstraintError(message: String)
 
 trait Constraint {
-  def validate(value: Option[String], messages: Map[String, String]): Seq[ConstraintError]
+  def validate(value: Option[String], messages: Map[String, String] = Map()): Seq[ConstraintError]
   def &(other: Constraint): Constraint = {
     val self = this
     (value: Option[String], messages: Map[String, String]) =>
---
+++ src/main/scala/org/make/core/validation/RegexContraint.scala
@@ -0,0 +1,17 @@
+package org.make.core.validation
+
+import scala.util.matching.Regex
+
+class RegexConstraint(pattern: Regex) extends Constraint {
+  override def validate(value: Option[String], constraintMessages: Map[String, String] = Map()): Seq[ConstraintError] = {
+
+    if (value.isEmpty) {
+      Seq()
+    } else {
+      pattern.findFirstIn(value.get) match {
+        case Some(_) => Seq()
+        case None    => Seq(ConstraintError(constraintMessages.get("invalid").getOrElse("Value not match")))
+      }
+    }
+  }
+}
