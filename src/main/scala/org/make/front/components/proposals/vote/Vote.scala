b2224970bf13187665201712760266af3db5d5e5
cecilebruzac <cb@make.org>
2017-09-20 11:42:39 +0200

    refactor(Vote): Align buttons

--- src/main/scala/org/make/front/components/proposals/vote/Vote.scala
+++ src/main/scala/org/make/front/components/proposals/vote/Vote.scala
@@ -6,8 +6,10 @@ import io.github.shogowada.scalajs.reactjs.classes.ReactClass
 import org.make.front.components.presentationals._
 import org.make.front.components.proposals.vote.VoteButton.VoteButtonProps
 import org.make.front.models.{Vote => VoteModel}
+import org.make.front.styles.ThemeStyles
 
 import scalacss.DevDefaults._
+import scalacss.internal.Length
 
 sealed trait VoteType
 
@@ -32,18 +34,18 @@ object Vote {
             g.console.log(vote.key.toString)
           }
 
-          <.ul()(
-            <.li()(
+          <.ul(^.className := VoteStyles.voteButtonsList)(
+            <.li(^.className := VoteStyles.voteButtonItem)(
               <.VoteButtonComponent(
                 ^.wrapped := VoteButtonProps(vote = self.props.wrapped.voteAgreeStats, handleVote = saveVote)
               )()
             ),
-            <.li()(
+            <.li(^.className := VoteStyles.voteButtonItem)(
               <.VoteButtonComponent(
                 ^.wrapped := VoteButtonProps(vote = self.props.wrapped.voteDisagreeStats, handleVote = saveVote)
               )()
             ),
-            <.li()(
+            <.li(^.className := VoteStyles.voteButtonItem)(
               <.VoteButtonComponent(
                 ^.wrapped := VoteButtonProps(vote = self.props.wrapped.voteNeutralStats, handleVote = saveVote)
               )()
@@ -55,4 +57,22 @@ object Vote {
 
 }
 
-object VoteStyles extends StyleSheet.Inline {}
+object VoteStyles extends StyleSheet.Inline {
+
+  import dsl._
+
+  //TODO: globalize function
+  implicit class NormalizedSize(val baseSize: Int) extends AnyVal {
+    def pxToEm(browserContextSize: Int = 16): Length[Double] = {
+      (baseSize.toFloat / browserContextSize.toFloat).em
+    }
+  }
+
+  val voteButtonsList: StyleA = style(
+    margin := s"0 ${(ThemeStyles.SpacingValue.small - ThemeStyles.SpacingValue.smaller).pxToEm().value}",
+    textAlign.center
+  )
+
+  val voteButtonItem: StyleA =
+    style(display.inlineBlock, verticalAlign.middle, margin(ThemeStyles.SpacingValue.smaller.pxToEm()))
+}
