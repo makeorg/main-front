f56b6b52de5e3a5438de496fc17d4e33fad87208
Philippe de Araujo <pa@make.org>
2017-10-05 10:58:10 +0200

    fix(auth): deactivate browser validation on forms

--- src/main/scala/org/make/front/facades/HexToRgba.scala
+++ src/main/scala/org/make/front/facades/HexToRgba.scala
@@ -5,7 +5,7 @@ import scalajs.js
 
 @js.native
 @JSImport("hex-to-rgba", JSImport.Default)
-object HexToRgba {
+object HexToRgba extends js.Any {
   def hexToRgba(rgb: String): String = js.native
   def hexToRgba(rgb: String, opacity: Float): String = js.native
 }
