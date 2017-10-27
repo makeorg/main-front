1dd76f283098d8b88137dc3bc5501b7acbb6c33f
cecilebruzac <cb@make.org>
2017-10-27 17:31:41 +0200

    feat(CookieAlert): dismiss alert

--- src/main/scala/org/make/front/components/cookieAlert/CookieAlertContainer.scala
+++ src/main/scala/org/make/front/components/cookieAlert/CookieAlertContainer.scala
@@ -6,6 +6,8 @@ import io.github.shogowada.scalajs.reactjs.redux.ReactRedux
 import io.github.shogowada.scalajs.reactjs.redux.Redux.Dispatch
 import org.make.front.components.AppState
 
+import org.make.front.facades.Cookies
+
 object CookieAlertContainer {
 
   final case class CookieAlertContainerProps()
@@ -14,6 +16,10 @@ object CookieAlertContainer {
 
   def selectorFactory: (Dispatch) => (AppState, Props[CookieAlertContainerProps]) => CookieAlert.CookieAlertProps =
     (_: Dispatch) => { (appState: AppState, ownProps: Props[CookieAlertContainerProps]) =>
-      CookieAlert.CookieAlertProps(isAlertOpened = true, closeCallback = () => {})
+      var isAlertOpened: Boolean = !Cookies.get("cookieconsent_status").isDefined
+      def dismissCookieAlert: () => Unit = { () =>
+        Cookies.set("cookieconsent_status", "dismiss")
+      }
+      CookieAlert.CookieAlertProps(isAlertOpened = isAlertOpened, closeCallback = dismissCookieAlert)
     }
 }
