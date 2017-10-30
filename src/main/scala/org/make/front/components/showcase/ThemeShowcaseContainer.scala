858dea518f887895fe9f1f31fd863734b666727d
Amine Zaghdoudi <amine.zaghdoudi@gmail.com>
2017-10-30 15:28:49 +0100

    fix(homepage): use label filter only for theme showcase

--- src/main/scala/org/make/front/components/showcase/ThemeShowcaseContainer.scala
+++ src/main/scala/org/make/front/components/showcase/ThemeShowcaseContainer.scala
@@ -5,6 +5,7 @@ import io.github.shogowada.scalajs.reactjs.classes.ReactClass
 import io.github.shogowada.scalajs.reactjs.redux.ReactRedux
 import io.github.shogowada.scalajs.reactjs.redux.Redux.Dispatch
 import org.make.front.components.AppState
+import org.make.front.models.Label
 import org.make.services.proposal.ProposalResponses.SearchResponse
 import org.make.services.proposal.ProposalService
 
@@ -26,7 +27,7 @@ object ThemeShowcaseContainer {
       maybeTheme.map { theme =>
         ThemeShowcase.ThemeShowcaseProps(
           proposals = ProposalService
-            .searchProposals(themesIds = Seq(theme.id), limit = Some(4), sort = Seq.empty, skip = None),
+            .searchProposals(themesIds = Seq(theme.id), labelsIds = Some(Seq(Label.Star.name)), limit = Some(4), sort = Seq.empty, skip = None),
           maybeTheme = Some(theme),
           maybeIntro = ownProps.wrapped.maybeIntro,
           maybeNews = ownProps.wrapped.maybeNews
