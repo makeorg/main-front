bf583f616619777ca988a7131adca91f42bf3a0a
cecilebruzac <cb@make.org>
2017-10-31 16:48:52 +0100

    fix(transverse): switch main header if vff sequence

--- src/main/scala/org/make/front/components/App.scala
+++ src/main/scala/org/make/front/components/App.scala
@@ -2,9 +2,10 @@ package org.make.front.components
 
 import io.github.shogowada.scalajs.reactjs.React
 import io.github.shogowada.scalajs.reactjs.VirtualDOM._
+import io.github.shogowada.scalajs.reactjs.router.RouterProps._
 import io.github.shogowada.scalajs.reactjs.router.WithRouter
 import org.make.front.components.Components.RichVirtualDOMElements
-import org.make.front.styles.base._
+import org.make.front.styles.base.{ColRulesStyles, RowRulesStyles, TextStyles, _}
 import org.make.front.styles.ui._
 
 import scalacss.DevDefaults._
@@ -12,27 +13,35 @@ import scalacss.DevDefaults._
 object App {
 
   lazy val reactClass = WithRouter(
-    React.createClass[Unit, Unit](
-      displayName = "App",
-      render = (_) =>
-        <("app-container")(^.className := "App")(
-          <.style()(
-            Basic.render[String],
-            RowRulesStyles.render[String],
-            ColRulesStyles.render[String],
-            TextStyles.render[String],
-            CTAStyles.render[String],
-            InputStyles.render[String],
-            TagStyles.render[String],
-            ModalStyles.render[String],
-            TooltipStyles.render[String]
-          ),
-          <.AboutMakeMainHeaderComponent.empty,
-          <.NotificationsComponent.empty,
-          <.ContainerComponent.empty,
-          <.MainFooterComponent.empty,
-          <.style()(RWDHideRulesStyles.render[String])
-      )
+    WithRouter(
+      React
+        .createClass[Unit, Unit](
+          displayName = "App",
+          render = (self) => {
+            <("app-container")(^.className := "App")(
+              <.style()(
+                Basic.render[String],
+                RowRulesStyles.render[String],
+                ColRulesStyles.render[String],
+                TextStyles.render[String],
+                CTAStyles.render[String],
+                InputStyles.render[String],
+                TagStyles.render[String],
+                ModalStyles.render[String],
+                TooltipStyles.render[String]
+              ),
+              if (self.props.location.pathname == "/consultation/comment-lutter-contre-les-violences-faites-aux-femmes") {
+                <.AboutMakeMainHeaderComponent.empty
+              } else {
+                <.MainHeaderComponent.empty
+              },
+              <.NotificationsComponent.empty,
+              <.ContainerComponent.empty,
+              <.MainFooterComponent.empty,
+              <.style()(RWDHideRulesStyles.render[String])
+            )
+          }
+        )
     )
   )
 }
--- src/main/scala/org/make/front/components/mainHeader/MainHeader.scala
+++ src/main/scala/org/make/front/components/mainHeader/MainHeader.scala
@@ -27,13 +27,15 @@ object MainHeader {
           <.div(^.className := ColRulesStyles.col)(
             <.div(^.className := MainHeaderStyles.innerWrapper)(
               <.p(^.className := MainHeaderStyles.logoWrapper)(
-                <.img(
-                  ^.className := MainHeaderStyles.logo,
-                  ^.src := logoMake.toString,
-                  ^.title := I18n.t("main-header.title"),
-                  ^.alt := I18n.t("main-header.title"),
-                  ^("data-pin-no-hover") := "true"
-                )()
+                <.Link(^.to := "/")(
+                  <.img(
+                    ^.className := MainHeaderStyles.logo,
+                    ^.src := logoMake.toString,
+                    ^.title := I18n.t("main-header.title"),
+                    ^.alt := I18n.t("main-header.title"),
+                    ^("data-pin-no-hover") := "true"
+                  )()
+                )
               ),
               <.div(^.className := MainHeaderStyles.searchWrapper)(<.SearchFormComponent.empty),
               <.div(^.className := MainHeaderStyles.menusWrapper)(
