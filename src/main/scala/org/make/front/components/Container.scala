de9b5101e4d8a43fea14aad05fe0cc503ef21b8c
Amine Zaghdoudi <az@make.org>
2018-02-27 11:02:16 +0100

    fix(operation): dynamise country not opened to core page

--- src/main/scala/org/make/front/components/Container.scala
+++ src/main/scala/org/make/front/components/Container.scala
@@ -7,7 +7,6 @@ import io.github.shogowada.scalajs.reactjs.router.dom.RouterDOM._
 import org.make.front.components.activateAccount.ActivateAccountContainer
 import org.make.front.components.authenticate.resetPassword.ResetPasswordContainer
 import org.make.front.components.currentOperations.CurrentOperationsContainer
-import org.make.front.components.currentlyRunning.CurrentlyRunningContainer
 import org.make.front.components.error.ErrorContainer
 import org.make.front.components.home.HomeContainer
 import org.make.front.components.maintenance.Maintenance
@@ -93,7 +92,7 @@ object Container {
           <.Route(^.exact := true, ^.path := "/", ^.render := { (_: React.Props[Unit]) =>
             <.Redirect(^.to := s"/$getDetectedCountry")()
           })(),
-          <.Route(^.exact := true, ^.path := "/:country/soon", ^.component := CurrentlyRunningContainer.reactClass)(),
+          <.Route(^.exact := true, ^.path := "/:country/soon", ^.component := CurrentOperationsContainer.reactClass)(),
           <.Route(^.exact := false, ^.path := "/", ^.component := ErrorContainer.reactClass)()
       )
     )
