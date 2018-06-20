e04df6f55d139a0eddc4622a94a8456a130d7e32
François LAROCHE <fl@make.org>
2018-06-20 16:46:12 +0200

    chore(transverse): simplify uglify options
    Do not set ecma6 since ie11 since to be unable to handle it
    
    Signed-off-by: François LAROCHE <fl@make.org>

--- make-webpack-prod.config.js
+++ make-webpack-prod.config.js
@@ -69,11 +69,6 @@ var build = {
         new UglifyJsPlugin({
             cache: true,
             parallel: true,
-            uglifyOptions: {
-              compress: false,
-              ecma: 6,
-              mangle: true
-            },
             sourceMap: true
           })
       ]
