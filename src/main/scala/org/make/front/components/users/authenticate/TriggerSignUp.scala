d56a8a8a922738128291d4b8b3ee4292a6b6af1c
Olivier Donze <od@make.org>
2018-09-21 10:43:11 +0200

    feat(user): Follow Button in Actor Profile

--- src/main/scala/org/make/front/components/users/authenticate/TriggerSignUp.scala
+++ src/main/scala/org/make/front/components/users/authenticate/TriggerSignUp.scala
@@ -25,14 +25,14 @@ import java.util.UUID
 import io.github.shogowada.scalajs.reactjs.React
 import io.github.shogowada.scalajs.reactjs.VirtualDOM.{<, ^}
 import io.github.shogowada.scalajs.reactjs.classes.ReactClass
-import org.make.front.components.authenticate.LoginOrRegister.LoginOrRegisterProps
 import org.make.front.components.Components._
+import org.make.front.components.authenticate.LoginOrRegister.LoginOrRegisterProps
 import org.make.front.components.modals.Modal.ModalProps
 import org.make.front.facades.I18n
 import org.make.front.facades.Unescape.unescape
 import org.make.front.middlewares.TriggerSignUpMiddleware
 import org.make.front.middlewares.TriggerSignUpMiddleware.TriggerSignUpListener
-import org.make.front.models.{Location, OperationId}
+import org.make.front.models.Location
 import org.make.front.models.Location.OperationPage
 import org.make.services.tracking.TrackingLocation
 import org.make.services.tracking.TrackingService.TrackingContext
@@ -57,10 +57,12 @@ object TriggerSignUp {
         )
       },
       componentDidMount = { self =>
-        val onTriggerSignUp: Location => Unit = { location =>
-          self.setState(state => state.copy(voteCount = state.voteCount + 1))
-          if (self.state.voteCount != 0 && self.state.voteCount % self.props.wrapped.nVotesTriggerConnexion == 0)
-            self.setState(_.copy(isAuthenticateModalOpened = true, voteLocation = Some(location)))
+        val onTriggerSignUp: (Location, Boolean) => Unit = {
+          case (location, forceSignUp) =>
+            if (!forceSignUp)
+              self.setState(state => state.copy(voteCount = state.voteCount + 1))
+            if (forceSignUp || self.state.voteCount != 0 && self.state.voteCount % self.props.wrapped.nVotesTriggerConnexion == 0)
+              self.setState(_.copy(isAuthenticateModalOpened = true, voteLocation = Some(location)))
         }
         TriggerSignUpMiddleware
           .addTriggerSignUpListener(self.state.id, TriggerSignUpListener(onTriggerSignUp))
